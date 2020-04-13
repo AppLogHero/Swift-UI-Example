e7ae8efcda2166c420948231fcd127a825556366
AppLog <julien.delferiere@gmail.com>
2020-04-13 16:11:09 +0200

    Initial commit

---
+++ The-Martin-Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The-Martin-Tutorial
+//
+//  Created by AppLog on 13/04/2020.
+//  Copyright © 2020 AppLog. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
